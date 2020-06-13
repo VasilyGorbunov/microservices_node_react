99030536cc0443dd962f25ea7542f4a07f00ae1e
VasilyGorbunov <vasily.gorbunov@gmail.com>
2020-06-13 15:59:11 +0300

    lesson 43

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -35,9 +30,24 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
-  res.send({});
-
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.listen(4002, console.log('QUERY listen on 4002'));
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
+  res.send({});
+});
+
+app.listen(4002, async () => {
+  console.log('QUERY listen on 4002');
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log(`Proccesing event: ${event.type}`);
+    handleEvent(event.type, event.data);
+  }
+});
